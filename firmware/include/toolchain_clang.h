44a0c561d03e3870c3c970da38cfb7315efd86a0
Mikaela Szekely <mikaela.szekely@qyriad.me>
2021-03-03 13:02:32 -0700

    Fix fixed-width integer format constants in toolchain.h
    The single-file printf we use doesn't support length specifiers other than l,
    which meant that format specifiers like PRIu8 as defined in inttypes.h didn't work.
    This commit redefines them so that linters like ccls will still be happy, while
    using specifiers like PRIu8 will, you know, actually work correctly, instead of
    just literally printing `%hhu`.

--- firmware/include/toolchain_clang.h
+++ firmware/include/toolchain_clang.h
@@ -8,9 +8,37 @@
 #define __LIBGREAT_TOOLCHAIN_COMPILER_H__
 
 #ifndef PRIu32
-#define PRIu32 "u"
-#define PRIx32 "x"
+
 #define PRIu8  "hhu"
+#define PRIu16 "hu"
+#define PRIu32 "lu"
+#define PRIu64 "llu"
+
+#define PRId8  "hhd"
+#define PRId16 "hd"
+#define PRId32 "ld"
+#define PRId64 "lld"
+
+#define PRIi8  "hhi"
+#define PRIi16 "hi"
+#define PRIi32 "li"
+#define PRIi64 "lli"
+
+#define PRIx8  "hhx"
+#define PRIx16 "hx"
+#define PRIx32 "lx"
+#define PRIx64 "llx"
+
+#define PRIX8  "hhX"
+#define PRIX16 "hX"
+#define PRIX32 "lX"
+#define PRIX64 "llX"
+
+#define PRIo8  "hho"
+#define PRIo16 "ho"
+#define PRIo32 "lo"
+#define PRIo64 "llo"
+
 #endif
 
 #endif
--- firmware/include/toolchain_gcc.h
+++ firmware/include/toolchain_gcc.h
@@ -7,6 +7,38 @@
 #ifndef __LIBGREAT_TOOLCHAIN_COMPILER_H__
 #define __LIBGREAT_TOOLCHAIN_COMPILER_H__
 
-#include <inttypes.h>
+#ifndef PRIu32
+
+#define PRIu8  "u"
+#define PRIu16 "u"
+#define PRIu32 "u"
+#define PRIu64 "u"
+
+#define PRId8  "d"
+#define PRId16 "d"
+#define PRId32 "d"
+#define PRId64 "d"
+
+#define PRIi8  "i"
+#define PRIi16 "i"
+#define PRIi32 "i"
+#define PRIi64 "i"
+
+#define PRIx8  "x"
+#define PRIx16 "x"
+#define PRIx32 "x"
+#define PRIx64 "x"
+
+#define PRIX8  "X"
+#define PRIX16 "X"
+#define PRIX32 "X"
+#define PRIX64 "X"
+
+#define PRIo8  "ho"
+#define PRIo16 "o"
+#define PRIo32 "o"
+#define PRIo64 "o"
+
+#endif
 
 #endif
